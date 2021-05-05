136b544c63e22d1b6fbe62a417ca8a81e9201843
Emil Madsen <emil@magenta.dk>
2021-05-05 17:14:10 +0200

    Initial commit

---
+++ ra_flatfile_importer/lora.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+# --------------------------------------------------------------------------------------
+from typing import List
+
+import click
+from pydantic import BaseModel, AnyHttpUrl
+from os2models.lora import Facet, Klasse, Organisation
+
+from util import model_validate_helper, takes_json_file, validate_url
+
+
+class LoraFlatFileFormatModel(BaseModel):
+    """Flatfile format for LoRa.
+
+    Minimal valid example is {}.
+    """
+    facetter: List[Facet] = []
+    klasser: List[Klasse] = []
+    organisationer: List[Organisation] = []
+
+
+def lora_validate_helper(json_file) -> LoraFlatFileFormatModel:
+    return model_validate_helper(LoraFlatFileFormatModel, json_file)
+
+
+@click.group()
+def lora() -> None:
+    """Lora Flatfile importer.
+
+    Used to validate and load flatfile data (JSON) into LoRa.
+    """
+    pass
+
+
+@lora.command()
+@takes_json_file
+def validate(json_file) -> None:
+    """Validate the provided JSON file."""
+    lora_validate_helper(json_file)
+
+
+@lora.command()
+@click.option("--indent", help="Pass 'indent' to json serializer", type=click.INT, default=None)
+def schema(indent: int) -> None:
+    """Generate JSON schema for validate files."""
+    click.echo(LoraFlatFileFormatModel.schema_json(indent=indent))
+
+
+@lora.command()
+@click.option(
+    '--mox-url',
+    default="http://localhost:8080",
+    show_default=True,
+    callback=validate_url,
+    help="Address of the MOX host",
+)
+@takes_json_file
+def upload(json_file, mox_url: AnyHttpUrl) -> None:
+    """Validate the provided JSON file and upload its contents."""
+    flatfilemodel = lora_validate_helper(json_file)
+    # TODO: Upload it using Client
---
+++ ra_flatfile_importer/mo.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+# --------------------------------------------------------------------------------------
+from typing import List
+
+import click
+from pydantic import BaseModel, AnyHttpUrl
+from os2models.mo import OrganisationUnit, Employee, Engagement, Address, Manager, EngagementAssociation
+
+from util import model_validate_helper, takes_json_file, validate_url
+
+
+class MOFlatFileFormatModel(BaseModel):
+    """Flatfile format for OS2mo.
+
+    Minimal valid example is {}.
+    """
+    org_units: List[OrganisationUnit] = []
+    employees: List[Employee] = []
+    engagements: List[Engagement] = []
+    address: List[Address] = []
+    manager: List[Manager] = []
+    engagement_associations: List[EngagementAssociation] = []
+
+
+def mo_validate_helper(json_file) -> MOFlatFileFormatModel:
+    return model_validate_helper(MOFlatFileFormatModel, json_file)
+
+
+@click.group()
+def mo() -> None:
+    """OS2mo Flatfile importer.
+
+    Used to validate and load flatfile data (JSON) into OS2mo.
+    """
+    pass
+
+
+@mo.command()
+@takes_json_file
+def validate(json_file) -> None:
+    """Validate the provided JSON file."""
+    mo_validate_helper(json_file)
+
+
+@mo.command()
+@click.option("--indent", help="Pass 'indent' to json serializer", type=click.INT, default=None)
+def schema(indent: int) -> None:
+    """Generate JSON schema for validate files."""
+    click.echo(MOFlatFileFormatModel.schema_json(indent=indent))
+
+
+@mo.command()
+@click.option(
+    '--mo-url',
+    default="http://localhost:5000",
+    show_default=True,
+    callback=validate_url,
+    help="Address of the OS2mo host",
+)
+@takes_json_file
+def upload(json_file, mo_url: AnyHttpUrl) -> None:
+    """Validate the provided JSON file and upload its contents."""
+    flatfilemodel = mo_validate_helper(json_file)
+    # TODO: Upload it using Client
