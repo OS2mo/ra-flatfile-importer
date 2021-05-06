45330a42c82a326c67d920c80d20c9851c264514
Emil Madsen <emil@magenta.dk>
2021-05-06 13:24:27 +0200

    Added LoRa base-file generator

--- ra_flatfile_importer/lora.py
+++ ra_flatfile_importer/lora.py
@@ -12,6 +12,7 @@ from pydantic import BaseModel
 from ramodels.lora import Facet
 from ramodels.lora import Klasse
 from ramodels.lora import Organisation
+from util import generate_uuid
 from util import model_validate_helper
 from util import takes_json_file
 from util import validate_url
@@ -74,3 +75,138 @@ def upload(json_file, mox_url: AnyHttpUrl) -> None:
     flatfilemodel = lora_validate_helper(json_file)
     print(flatfilemodel)
     # TODO: Upload it using Client
+
+
+@lora.command()
+@click.option(
+    "--name",
+    help="Name of the root organization",
+    required=True,
+)
+@click.option(
+    "--indent", help="Pass 'indent' to json serializer", type=click.INT, default=None
+)
+def initialize(name: str, indent: int) -> None:
+    """Generate LoRa initializer file."""
+    seed = name
+    org_uuid = generate_uuid(seed)
+    organisation = Organisation.from_simplified_fields(
+        uuid=org_uuid,
+        name=name,
+        user_key=name,
+    )
+    org_unit_address_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "org_unit_address_type"),
+        user_key="org_unit_address_type",
+        organisation_uuid=org_uuid,
+    )
+    employee_address_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "employee_address_type"),
+        user_key="employee_address_type",
+        organisation_uuid=org_uuid,
+    )
+    address_property = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "address_property"),
+        user_key="address_property",
+        organisation_uuid=org_uuid,
+    )
+    engagement_job_function = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "engagement_job_function"),
+        user_key="engagement_job_function",
+        organisation_uuid=org_uuid,
+    )
+    org_unit_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "org_unit_type"),
+        user_key="org_unit_type",
+        organisation_uuid=org_uuid,
+    )
+    engagement_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "engagement_type"),
+        user_key="engagement_type",
+        organisation_uuid=org_uuid,
+    )
+    engagement_association_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "engagement_association_type"),
+        user_key="engagement_association_type",
+        organisation_uuid=org_uuid,
+    )
+    association_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "association_type"),
+        user_key="association_type",
+        organisation_uuid=org_uuid,
+    )
+    role_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "role_type"),
+        user_key="role_type",
+        organisation_uuid=org_uuid,
+    )
+    leave_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "leave_type"),
+        user_key="leave_type",
+        organisation_uuid=org_uuid,
+    )
+    manager_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "manager_type"),
+        user_key="manager_type",
+        organisation_uuid=org_uuid,
+    )
+    responsibility = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "responsibility"),
+        user_key="responsibility",
+        organisation_uuid=org_uuid,
+    )
+    manager_level = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "manager_level"),
+        user_key="manager_level",
+        organisation_uuid=org_uuid,
+    )
+    visibility = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "visibility"),
+        user_key="visibility",
+        organisation_uuid=org_uuid,
+    )
+    time_planning = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "time_planning"),
+        user_key="time_planning",
+        organisation_uuid=org_uuid,
+    )
+    org_unit_level = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "org_unit_level"),
+        user_key="org_unit_level",
+        organisation_uuid=org_uuid,
+    )
+    primary_type = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "primary_type"),
+        user_key="primary_type",
+        organisation_uuid=org_uuid,
+    )
+    org_unit_hierarchy = Facet.from_simplified_fields(
+        uuid=generate_uuid(seed + "org_unit_hierarchy"),
+        user_key="org_unit_hierarchy",
+        organisation_uuid=org_uuid,
+    )
+
+    flatfile = LoraFlatFileFormatModel(
+        facetter=[
+            org_unit_address_type,
+            employee_address_type,
+            address_property,
+            engagement_job_function,
+            org_unit_type,
+            engagement_type,
+            engagement_association_type,
+            association_type,
+            role_type,
+            leave_type,
+            manager_type,
+            responsibility,
+            manager_level,
+            visibility,
+            time_planning,
+            org_unit_level,
+            primary_type,
+            org_unit_hierarchy,
+        ],
+        organisationer=[organisation],
+    )
+    click.echo(flatfile.json(indent=indent))
