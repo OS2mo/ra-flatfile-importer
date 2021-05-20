939924e172b61c675129f87731a276ad7101fddc
Emil Madsen <sovende@gmail.com>
2021-05-20 15:20:36 +0200

    Reformatting

--- ra_flatfile_importer/__init__.py
+++ ra_flatfile_importer/__init__.py
@@ -3,14 +3,11 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
-from typing import List
-
-from ra_flatfile_importer.semantic_version_type import SemanticVersion
-
-from ra_flatfile_importer.mo_flatfile_model import MOFlatFileFormat
-from ra_flatfile_importer.mo_flatfile_model import MOFlatFileFormatChunk
 from ra_flatfile_importer.lora_flatfile_model import LoraFlatFileFormat
 from ra_flatfile_importer.lora_flatfile_model import LoraFlatFileFormatChunk
+from ra_flatfile_importer.mo_flatfile_model import MOFlatFileFormat
+from ra_flatfile_importer.mo_flatfile_model import MOFlatFileFormatChunk
+from ra_flatfile_importer.semantic_version_type import SemanticVersion
 
 __version__: SemanticVersion = SemanticVersion("0.1.0")
 
