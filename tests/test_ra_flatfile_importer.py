4fa6aee6d79ac220739de9cd775cc4a1da084a9d
Emil Madsen <sovende@gmail.com>
2021-05-28 11:35:47 +0200

    Added version test

--- tests/test_ra_flatfile_importer.py
+++ tests/test_ra_flatfile_importer.py
@@ -3,10 +3,18 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
+from importlib.metadata import version  # type: ignore
+
+from ra_flatfile_importer import __version__
 from ra_flatfile_importer import LoraFlatFileFormat
 from ra_flatfile_importer import MOFlatFileFormat
 from ra_flatfile_importer.lora_flatfile_gen import generate_lora_flatfile
 from ra_flatfile_importer.mo_flatfile_gen import generate_mo_flatfile
+
+
+def test_version():
+    pyproject_version = version("ra-flatfile-importer")
+    assert __version__ == pyproject_version
 
 
 def test_lora_generate():
